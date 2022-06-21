b9a11f2761bb68858562f36b53d3a15f7e7369ff
RoshanAli339 <roshanali339lps@gmail.com>
2022-06-21 22:35:23 +0530

    Tuesday 21 June 2022 10:35:23 PM IST

--- lab_8_a.py
+++ lab_8_a.py
@@ -1,11 +1,13 @@
+import gc
+
+
 def gcd(n1, n2):
     if n2 != 0:
-        gcd(n2, n1 % n2)
+        return gcd(n2, n1 % n2)
     else:
         return n1
 
 
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
-
 print(f"GCD of {n1} and {n2}: ", gcd(n1, n2))
