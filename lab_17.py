ffc9b35316af9ae07258cde6db24ec47479c36a9
RoshanAli339 <roshanali339lps@gmail.com>
2022-06-27 19:35:32 +0530

    Monday 27 June 2022 07:35:32 PM IST

--- lab_17.py
+++ lab_17.py
@@ -13,7 +13,7 @@ print("The popped out value is: ", x)
 a = input("Enter a unit to update the dict: ")
 x = input("Enter the value to update with: ")
 d.update({a: x})
-print("The updates dictionary is: ", d)
+print("The updated dictionary is: ", d)
 
 print("The values are: ", d.values())
 
