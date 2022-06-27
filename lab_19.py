6db136b4601f9a727c10d95e091e5ef9807de76c
RoshanAli339 <roshanali339lps@gmail.com>
2022-06-27 19:52:54 +0530

    Monday 27 June 2022 07:52:54 PM IST

--- lab_19.py
+++ lab_19.py
@@ -5,4 +5,4 @@ x = int(input("Enter the element to insert in the tuple: "))
 pos = int(input("Enter the position to insert the element: "))
 
 t = t[:pos-1] + (x, ) + t[pos-1:]
-print("After the inserting the elements of the tuple are: ", t)
+print("After inserting the elements of the tuple are: ", t)
